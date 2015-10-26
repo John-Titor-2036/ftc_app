3b76bc7189ba3751d6fe6281b6a587f81e0a0f68
Ali Rahman <alirahm99@gmail.com>
2015-10-25 19:39:24 -0500

    Sensor fix
    GG Jerry

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SensorPlayground.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SensorPlayground.java
@@ -21,7 +21,9 @@ public class SensorPlayground extends OpMode implements SensorEventListener {
 
 
     double degree = 0;
-
+	float azimut;
+	float pitch;
+	float roll;
 	public SensorPlayground() {
 
 	}
@@ -40,7 +42,9 @@ public class SensorPlayground extends OpMode implements SensorEventListener {
 	public void loop() {
 
 
-
+		telemetry.addData("text", "Azimut: " + azimut);
+		telemetry.addData("text", "Pitch: " + pitch);
+		telemetry.addData("text", "Roll: " + roll);
 
 
     }
@@ -51,12 +55,25 @@ public class SensorPlayground extends OpMode implements SensorEventListener {
 	}
 
     @Override
-    public void onSensorChanged (SensorEvent event) {
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+			mGravity = event.values;
+		if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+			mGeomagnetic = event.values;
+		if (mGravity != null && mGeomagnetic != null) {
+			float R[] = new float[9];
+			float I[] = new float[9];
+			boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+			if (success) {
+				float orientation[] = new float[3];
+				SensorManager.getOrientation(R, orientation);
+				azimut = orientation[0]; // orientation contains: azimut, pitch and roll
+				pitch = orientation[1];
+				roll = orientation[2];
 
-        //degree = Math.round(event.values[0]);
-        telemetry.addData("Text", "Degrees: " + event.values[0]);
-
-    }
+			}
+		}
+	}
 
 
 	public void onAccuracyChanged(Sensor sensor, int accuracy)
